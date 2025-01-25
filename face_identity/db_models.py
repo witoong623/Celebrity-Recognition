04d92b15c993720b5f55951060f0584fc9381da2
Witoon Wiphusitphunpol <tenshi623@gmail.com>
2025-01-25 20:44:52 +0700

    add database model to store data

---
+++ face_identity/db_models.py
@@ -0,0 +1,46 @@
+from sqlalchemy import create_engine, String, LargeBinary, Float, Integer, ForeignKey
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column, relationship
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Image(Base):
+    __tablename__ = 'images'
+
+    id = mapped_column(Integer, primary_key=True)
+    image_path = mapped_column(String, nullable=False,
+                               doc='Relative path of image relative to dataset root directory')
+    # TODO: need to make it indexed
+    celeb_id = mapped_column(Integer, nullable=False)
+
+    faces = relationship(back_populates="image")
+
+
+class Face(Base):
+    __tablename__ = 'faces'
+
+    id = mapped_column(Integer, primary_key=True)
+    image_id = mapped_column(ForeignKey("images.id"))
+    face_image_path = mapped_column(String, nullable=False,
+                                    doc='Relative path of face image relative to dataset root directory')
+    face_embedding = mapped_column(LargeBinary, nullable=True)
+    facial_area = mapped_column(String, nullable=False,
+                                doc='JSON string of x, y, w, h, left_eye and right_eye')
+    confidence = mapped_column(Float, nullable=False)
+
+    image = relationship("Image", back_populates="faces")
+
+
+engine = None
+
+
+def get_session_maker(db_url) -> sessionmaker:
+    global engine
+    if engine is None:
+        engine = create_engine(db_url)
+        Base.metadata.create_all(engine)
+        print('Database tables created')
+
+    return sessionmaker(autocommit=False, autoflush=False, bind=engine)

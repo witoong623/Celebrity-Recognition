7ad9a3e49f8b8b5c8aa75547120c53bde12930cf
Witoon Wiphusitphunpol <tenshi623@gmail.com>
2025-01-26 18:24:49 +0700

    modify build face embedding script to use config

--- face_identity/build_face_embedding.py
+++ face_identity/build_face_embedding.py
@@ -1,31 +1,40 @@
+import argparse
+from pathlib import Path
+
 import numpy as np
 from deepface import DeepFace
 from sqlalchemy import select
 
-from db_models import DatasetSplit, Image as ImageModel, Face, get_session_maker
+from config import get_config
+from db_models import Face, get_session_maker
 
 
+parser = argparse.ArgumentParser(description='Detect faces in an image, then save to file and DB')
+parser.add_argument('--config', default='config.yaml', help='Config file path')
+args = parser.parse_args()
 
-session_maker = get_session_maker('sqlite:///face_identity.db')
+config = get_config(args.config)
+face_data_dir = Path(config.preprocessed_dataset_dir)
 
-# input size is 160x160, embedding size is 128
-FACE_MODEL = 'Facenet'
+session_maker = get_session_maker(config.get_db_url())
 
 with session_maker() as session:
     faces = session.execute(select(Face)).scalars()
 
     for face in faces:
-        img_path = face.face_image_path
+        img_path = face_data_dir / face.face_image_path
         embedding_obj = DeepFace.represent(
             img_path=img_path,
-            model_name=FACE_MODEL,
+            model_name=config.embedding_model,
             enforce_detection=False,
+            # we already detected faces, so skip detection
             detector_backend='skip',
-            normalization='Facenet'
+            # normalization use the same name as the model
+            normalization=config.embedding_model
         )
 
-        # embedding is list of 128 float values
-        # originally, shape is (1, 128), dtype is float32
+        # embedding is list of D float values where D is embedding size
+        # originally, shape is (1, D), dtype is float32
         face.face_embedding = np.array(embedding_obj[0]['embedding'], dtype=np.float32).tobytes()
 
         session.flush()
